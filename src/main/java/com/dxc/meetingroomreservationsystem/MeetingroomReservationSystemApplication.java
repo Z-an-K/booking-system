e9deae00d69c0bfbed54e83af96a286c80db5fe0
zhan7 <zhang.han@dxc.com>
2024-04-06 12:56:49 +0800

    initial commit

---
+++ src/main/java/com/dxc/meetingroomreservationsystem/MeetingroomReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dxc.meetingroomreservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingroomReservationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingroomReservationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dxc/meetingroomreservationsystem/MeetingroomReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxc.meetingroomreservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingroomReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
